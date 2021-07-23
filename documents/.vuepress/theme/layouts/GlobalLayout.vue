244911b967c621654917e855c9226d6d7e439574
kim-eun-ji <qwert101311@gmail.com>
2021-07-23 18:05:31 +0900

    Fix utterances error

--- documents/.vuepress/theme/layouts/GlobalLayout.vue
+++ documents/.vuepress/theme/layouts/GlobalLayout.vue
@@ -6,15 +6,32 @@
 </template>
 
 <script>
+import ParentLayout from "@vuepress/theme-default/layouts/Layout.vue";
+import Comment from "../../components/Comment.vue";
 export default {
   updated() {
-    if (document.getElementsByClassName("utterances")[0]) {
+    if (document.getElementById("comment_con")) {
       if (window.location.pathname === "/TIL/") {
-        document.getElementsByClassName("utterances")[0].style.display = "none";
+        document.getElementById("comment_con").style.display = "none";
       } else {
-        document.getElementsByClassName("utterances")[0].style.display = "block";
+        document.getElementById("comment_con").style.display = "block";
+
+        if (document.querySelector("iframe.utterances-frame")) {
+          const f = document.querySelector("iframe.utterances-frame");
+          let srcList = f.src.split("&");
+
+          srcList[5] = "url=" + encodeURIComponent(location.href);
+          srcList[7] = "pathname=" + encodeURIComponent(location.pathname.substr(1).replace(".html", ""));
+          srcList[8] = "title=" + encodeURIComponent(document.getElementsByTagName("title")[0].text);
+          srcList = srcList.join("&");
+          f.setAttribute("src", srcList);
+        }
       }
     }
+  },
+  components: {
+    ParentLayout,
+    Comment,
   },
 };
 </script>
